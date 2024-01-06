609815f60da02a66ee613632dbb47c05c454a2bd
IvaZach <ivazacherniuk@gmail.com>
2024-01-06 16:02:43 +0300

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,39 @@
-import { fetchDogByBreed } from 'api';
-import { BreedSelect } from 'components/BreedSelect/BreedSelect';
-import { GlobalStyle } from 'components/GlobalStyle';
-import { Layout } from 'components/Layout';
-import { HTTP_ERROR_MSG } from 'constants';
-import { DogDetails } from 'DogDetails';
-import { DogSkeleton } from 'DogSkeleton';
-import { ErrorMessage } from 'ErrorMessage';
-import React, { Component } from 'react';
+import { useEffect, useState } from 'react';
+import { RecipeList } from './RecipeList/RecipeList';
+import initialRecipes from '../recipes.json';
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout/Layout';
+import { RecipeForm } from './RecipeForm/RecipeForm';
 
-export class App extends Component {
-  state = {
-    dog: null,
-    loading: false,
-    error: '',
-  };
-
-  fetchDog = async breedId => {
-    try {
-      this.setState({ loading: true, error: null });
-      const fetchedDog = await fetchDogByBreed(breedId);
-      this.setState({ dog: fetchedDog });
-    } catch (error) {
-      this.setState({ error: HTTP_ERROR_MSG });
-    } finally {
-      this.setState({ loading: false });
-    }
-  };
-
-  render() {
-    const { dog, error, loading } = this.state;
-    return (
-      <Layout>
-        <BreedSelect onSelect={this.fetchDog} />
-        {dog && !loading && <DogDetails dog={dog} />}
-        {loading && <DogSkeleton/>}
-        {error && <ErrorMessage>{error}</ErrorMessage>}
-        <GlobalStyle />
-      </Layout>
-    );
+const getRecipes = () => {
+  const savedRecipes = localStorage.getItem('recipes');
+  if (savedRecipes !== null) {
+    return JSON.parse(savedRecipes);
+  } else {
+    return initialRecipes;
   }
-}
+};
+
+export const App = () => {
+  const [recipes, setRecipes] = useState(getRecipes);
+
+  useEffect(() => {
+    localStorage.setItem('recipes', JSON.stringify(recipes));
+  }, [recipes]);
+
+  const addRecipe = newRecipe => {
+    setRecipes(prevState => [...prevState, newRecipe]);
+  };
+
+  const deleteRecipe = recipeId => {
+    setRecipes(prevState => prevState.filter(recipe => recipe.id !== recipeId));
+  };
+
+  return (
+    <Layout>
+      <RecipeForm onSave={addRecipe} />
+      <RecipeList items={recipes} onDelete={deleteRecipe} />
+      <GlobalStyle />
+    </Layout>
+  );
+};
